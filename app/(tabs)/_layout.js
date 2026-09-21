c2be7e1df2bfc268a9a68e9c3b075d1c2c9dbc9b
Gustavo Victor <ferreirag930@yahoo.com>
2026-09-21 14:32:21 -0300

    termina GET

---
+++ app/(tabs)/_layout.js
@@ -0,0 +1,31 @@
+import { Tabs } from "expo-router";
+
+export default function TabsLayout() {
+  return (
+    <Tabs
+      screenOptions={{
+        headerShadowVisible: false,
+        tabBarActiveTintColor: "#0f62fe",
+        tabBarLabelStyle: {
+          fontSize: 12,
+          fontWeight: "600",
+        },
+      }}
+    >
+      <Tabs.Screen
+        name="index"
+        options={{
+          title: "Início",
+          headerTitle: "Projeto Base",
+        }}
+      />
+      <Tabs.Screen
+        name="aulas"
+        options={{
+          title: "Aulas",
+          headerTitle: "Conteúdo",
+        }}
+      />
+    </Tabs>
+  );
+}
---
+++ app/(tabs)/get.js
@@ -0,0 +1,35 @@
+import axios from "axios";
+import { useEffect, useState } from 'react';
+import { TurboModuleRegistry } from 'react-native';
+
+const API_KEY = 'cv_HsSx_oeiJ882GCghHPhmTfdFB0kphgB99xkaEQVCWv_KqI32BA_Y4x4V2ouZlee9';
+
+const api = axios.create({
+    baseURL: "https://api-ds.codeverse.com.br",
+    headers: {
+        "x-api-key": API_KEY,
+    },
+});
+
+export default function destinosListarScreen() {
+    const { destinos, setDestinos } = useState([])
+    const { carregando, setCarregando } = useState(true)
+    const { erro, setErro } = useState(null)
+}
+
+async function buscarDestinos() {
+    setCarregando(true)
+    setErro(null)
+    try {
+        const resposta = await api.get("/api/destinos");
+        setDestinos(resposta.data.data);
+    } catch (error) {
+        setErro("Erro ao buscar destinos");
+    } finally {
+        setCarregando(false)
+    };
+}
+
+useEffect(() => {
+    buscarDestinos()
+}, [])
